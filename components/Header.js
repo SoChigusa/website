3a3f5ad0852500a017390cc399b1fffa678da4e0
So Chigusa <so.chigusa.pp@gmail.com>
2022-08-29 15:38:02 -0700

    navbar layout

--- components/Header.js
+++ components/Header.js
@@ -8,17 +8,27 @@ export default function Header() {
         <Navbar.Brand href="/">So Chigusa</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/cv">CV</Nav.Link>
-            <Nav.Link href="/research">Research</Nav.Link>
-            <Nav.Link href="/note">Note</Nav.Link>
+          <Nav className="w-50 nav-justified">
+            <Nav.Item>
+              <Nav.Link href="/">Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/cv">CV</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/research">Research</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/note">Note</Nav.Link>
+            </Nav.Item>
             <NavDropdown title="Tips" id="basic-nav-dropdown">
               <NavDropdown.Item href="/tips">List</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="/tips/test2">test2</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="/git">Git</Nav.Link>
+            <Nav.Item>
+              <Nav.Link href="/git">Git</Nav.Link>
+            </Nav.Item>
           </Nav>
         </Navbar.Collapse>
       </Container>
