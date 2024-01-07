8ef06082239a5d21612aa6defcb9add89dc9f52f
SoChigusa <so.chigusa.pp@gmail.com>
2024-01-07 22:19:54 +0900

    bug fixed

--- utils/extractTalkData.js
+++ utils/extractTalkData.js
@@ -1,7 +1,8 @@
 const extractTalkData = async ({ slice = -1, separate = false } = {}) => {
-  const json = await fetch('https://script.google.com/macros/s/AKfycbxWGRs44fr2ZfokuqmOOVsgWlYpi9d_6h7Z6C5GRCyEweVxY50F3TH-bRZrOG6OaF1R9w/exec', { method: 'GET' }).then(data => {
+  let json = await fetch('https://script.google.com/macros/s/AKfycbxWGRs44fr2ZfokuqmOOVsgWlYpi9d_6h7Z6C5GRCyEweVxY50F3TH-bRZrOG6OaF1R9w/exec', { method: 'GET' }).then(data => {
     return data.json();
   });
+  json = json.filter(talk => talk.Type != '');
   if (separate) {
     let seminars = json.filter(talk => talk.Type == 'Seminar').reverse();
     let talks = json.filter(talk => (talk.Type != 'Seminar' && talk.Type != 'Award')).reverse();
